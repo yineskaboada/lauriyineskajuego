94a3d11a6fef635c8a7bdfcdea60df19651af46b
yineskaboada <vanesaruiz459@gmail.com>
2021-08-06 14:40:51 -0500

    proyectoly

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          mi nombre es lauris
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=Rom2yhkW1YU"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ src/square.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+ class Square extends React.Component {
+     render() {
+         return(
+             <button className="square">
+                {this.props.value}
+            </button>
+
+         );
+     }
+ }
+
+ export default Square;
